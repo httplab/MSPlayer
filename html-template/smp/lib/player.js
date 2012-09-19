a894e617d64a05b0f6ec987185ea5dc0ee6d7d32
rainrb <alex.vasyutin@gmail.com>
2012-09-19 19:05:25 +0400

    Initial commit

---
+++ html-template/smp/lib/player.js
@@ -0,0 +1,52 @@
+(function(){
+
+	if (!window.PLAYER) {
+		window.PLAYER = {};
+	}
+
+	PLAYER = {
+
+		init: function(params) {
+		    if (!params.src) {
+		    	console.log("Ошибка инициализации: не указано видео")
+		    	return false;
+		    }
+
+			// Формирование id контейнера для плеера
+			var containerId;
+			if (params.id) {
+				containerId = '#' + params.id
+				delete params['id']
+			} else {
+				containerId = '#player'
+				console.log("Не указан id контейнера плеера! Используется #player")
+			}
+
+			// Формирование перечня параметров плеера
+			var options = {
+				swf: 'MSPlayer.swf',
+				id: 'MSPlayerId',
+				src: params.src,
+				width: params.width || 640,
+				height:  params.height || 462,
+				enableStageVideo: true,
+				controlBarAutoHide: params.controlBarAutoHide || false,
+				playButtonOverlay: true,
+				autoPlay: params.autoPlay || true,
+				showVideoInfoOverlayOnStartUp: params.showVideoInfo || false
+			}
+			// Инициализация рекламы
+			if (params.ads) {
+				delete params['ads']
+				// options['plugin_ads'] = BASEPATH + "plugins/AdvertisementPlugin.swf"
+				// options = $.extend({}, options, params.ads);
+			}
+			PLAYER.embedPlayer(containerId, options);
+		},
+
+		embedPlayer: function(containerId, options) {
+			var $strobemediaplayback = $(containerId);
+			$player = $strobemediaplayback.strobemediaplayback(options);
+		}
+	}
+}).call(this);
